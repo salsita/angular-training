cbb0efc0bce790c68325e390dd1132d664b78f04
Adam Petrovič <adam@salsitasoft.com>
2018-01-26 09:25:23 +0100

    Redirect all unknown URLs to `/users`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
   {
     path: 'users',
     loadChildren: './users/users.module#UsersModule'
-  }
-  // { path: '**', component: NotFoundPageComponent },
+  },
+  { path: '**', redirectTo: '/users' }
 ];
 
 @NgModule({
